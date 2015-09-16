204638300e73f49205de14bdc89f89e41ec0dfef
eshiho <shiho.elliptic@gmail.com>
2015-09-16 21:32:19 +0900

    Fix rabin cipher bug

--- scryptos/crypto/rabin.py
+++ scryptos/crypto/rabin.py
@@ -1,4 +1,4 @@
-from scryptos.math.arithmetic import isqrt, modinv, chinese_remainder_theorem
+from scryptos.math.arithmetic import nth_root, modinv, chinese_remainder_theorem
 
 class Rabin:
   def __init__(s, **kwargs):
@@ -18,13 +18,13 @@ class Rabin:
     return m * (m + s.b) % s.n
 
   def decrypt(s, c):
-    mp = isqrt(c + s.b % s.p * (s.b * modinv(4, s.p)) % s.n) - (s.b * modinv(2, s.p)) % s.p
-    mq = isqrt(c + s.b % s.q * (s.b * modinv(4, s.q)) % s.n) - (s.b * modinv(2, s.q)) % s.q
+    mp = nth_root((c + (s.b**2 * modinv(4, s.p))) ** (s.p + 1), 4) - (s.b * modinv(2, s.p)) % s.p
+    mq = nth_root((c + (s.b**2 * modinv(4, s.q))) ** (s.q + 1), 4) - (s.b * modinv(2, s.q)) % s.q
     m1 = chinese_remainder_theorem([[mp, s.p], [mq, s.q]])
-    m2 = chinese_remainder_theorem([[-mp-b % s.p, s.p], [mq, s.q]])
-    m3 = chinese_remainder_theorem([[mp, s.p], [-mq-b % s.q, s.q]])
-    m4 = chinese_remainder_theorem([[-mp-b % s.p, s.p], [-mq-b % s.q, s.q]])
-    return [m1, m2, m3, m4]
+    m2 = chinese_remainder_theorem([[-mp-s.b % s.p, s.p], [mq, s.q]])
+    m3 = chinese_remainder_theorem([[mp, s.p], [-mq-s.b % s.q, s.q]])
+    m4 = chinese_remainder_theorem([[-mp-s.b % s.p, s.p], [-mq-s.b % s.q, s.q]])
+    return (m1, m2, m3, m4)
 
   def __getitem__(s, k):
     cond = {
@@ -37,5 +37,5 @@ class Rabin:
       return cond[k]
 
   def __repr__(s):
-    return "Rabin %s Key: (n: %d%s)" % (s.has_private()and"Private"or"Public", s.n, s.has_private()and(", p: %d, q: %d, x: %d"%(s.p, s.q, s.x))or"")
+    return "Rabin %s Key: (n: %d%s)" % (s.has_private()and"Private"or"Public", s.n, s.has_private()and(", p: %d, q: %d"%(s.p, s.q))or"")
 

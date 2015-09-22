2bf184a4a5acfbba0dcb9ba7b48aded9d570b3d2
eshiho <shiho.elliptic@gmail.com>
2015-09-22 11:44:52 +0900

    p64, u64

--- scryptos/util/hexutil.py
+++ scryptos/util/hexutil.py
@@ -2,9 +2,11 @@ import binascii, struct
 
 unhex = lambda x: int(x.replace(" ", "").replace("\t", "").replace("\n", ""), 16),
 bswap = lambda x: unhex(hex(x).decode("hex")[::-1]),
+p64   = lambda x: struct.pack("<Q", x),
 p32   = lambda x: struct.pack("<I", x),
 p16   = lambda x: struct.pack("<H", x),
 p8    = lambda x: struct.pack("<B", x),
+u64   = lambda x: struct.unpack("<Q", x)[0],
 u32   = lambda x: struct.unpack("<I", x)[0],
 u16   = lambda x: struct.unpack("<H", x)[0],
 u8    = lambda x: struct.unpack("<B", x)[0],
@@ -14,4 +16,4 @@ def hex(x, align=1):
   r = format(x, "x")
   if not len(r) % align == 0:
     r = ("0" * (align - len(r)%align)) + r
-  return r
+  return "0x" + r

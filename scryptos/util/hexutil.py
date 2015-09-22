01f8b2b1e747d5b24de45a800e481a2b1c7e428f
eshiho <shiho.elliptic@gmail.com>
2015-09-22 11:47:28 +0900

    fix "as tuple"

--- scryptos/util/hexutil.py
+++ scryptos/util/hexutil.py
@@ -1,15 +1,15 @@
 import binascii, struct
 
-unhex = lambda x: int(x.replace(" ", "").replace("\t", "").replace("\n", ""), 16),
-bswap = lambda x: unhex(hex(x).decode("hex")[::-1]),
-p64   = lambda x: struct.pack("<Q", x),
-p32   = lambda x: struct.pack("<I", x),
-p16   = lambda x: struct.pack("<H", x),
-p8    = lambda x: struct.pack("<B", x),
-u64   = lambda x: struct.unpack("<Q", x)[0],
-u32   = lambda x: struct.unpack("<I", x)[0],
-u16   = lambda x: struct.unpack("<H", x)[0],
-u8    = lambda x: struct.unpack("<B", x)[0],
+unhex = lambda x: int(x.replace(" ", "").replace("\t", "").replace("\n", ""), 16)
+bswap = lambda x: unhex(hex(x).decode("hex")[::-1])
+p64   = lambda x: struct.pack("<Q", x)
+p32   = lambda x: struct.pack("<I", x)
+p16   = lambda x: struct.pack("<H", x)
+p8    = lambda x: struct.pack("<B", x)
+u64   = lambda x: struct.unpack("<Q", x)[0]
+u32   = lambda x: struct.unpack("<I", x)[0]
+u16   = lambda x: struct.unpack("<H", x)[0]
+u8    = lambda x: struct.unpack("<B", x)[0]
 
 
 def hex(x, align=1):

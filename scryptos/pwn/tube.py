8de316ffb223c9e831936c1ca2fb71381a2c93c2
Shiho Midorikawa <shiho.elliptic@gmail.com>
2015-12-14 02:18:44 +0900

    use threading, events

--- scryptos/pwn/tube.py
+++ scryptos/pwn/tube.py
@@ -1,4 +1,5 @@
 from subprocess import Popen, PIPE, STDOUT
+from threading  import Thread, Event
 import select
 import socket
 import os
@@ -7,24 +8,13 @@ import fcntl
 import telnetlib
 
 class Tube:
-  def __init__(s, filename="", host="", port=-1, stderr=STDOUT, args=[], debug=False):
-    def setNonBlocking(fobj):
-      f = fcntl.fcntl(fobj, fcntl.F_GETFL)
-      f |= os.O_NONBLOCK
-      fcntl.fcntl(fobj, fcntl.F_SETFL, f)
-
+  def __init__(s, filename="", host="", port=-1, args=[], debug=False, auto_argv_0=True):
+    s.debug = debug
     if filename != "":
-      s.t = "POPEN"
-      if args == []:
-        args = [filename]
-      s.p = Popen(args, stdin=PIPE, stdout=PIPE, stderr=stderr, bufsize=1)
-      setNonBlocking(s.p.stdout)
-      setNonBlocking(s.p.stdin)
-      print "\x1b[0;32m[+] Start Process: %s args: %s PID=%d\x1b[0m" % (filename, repr(args), s.p.pid)
-      if debug:
-        raw_input("\x1b[0;32mPlease enter key to continue...\x1b[0m")
+      if auto_argv_0:
+        args = [filename] + args
+      s.proc_sock(args)
     elif host != "" and port != -1:
-      s.t = "SOCKET"
       s.p = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
       s.p.connect((host, port))
       s.p.setblocking(0)
@@ -32,23 +22,35 @@ class Tube:
     else:
       raise Exception("Invalid Argument")
 
+  def proc_sock(s, args):
+    def run_server(s, e, argv):
+      c, _ = s.proc_sock.accept()
+      s.proc_sock.close()
+      p = Popen(argv, stdin=c, stdout=c, stderr=c)
+      print "\x1b[0;32m[+] Start Process: %s args: %s PID=%d\x1b[0m" % (args[0], repr(args), p.pid)
+      if s.debug:
+        raw_input("\x1b[0;32mPlease enter key to continue...\x1b[0m")
+      e.set()
+      p.wait()
+      c.close()
+    s.proc_sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    s.proc_sock.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+    s.proc_sock.bind(("", 0))
+    s.proc_sock.listen(1)
+    e = Event()
+    t = Thread(target=run_server, args=(s, e, args))
+    t.start()
+    s.p = socket.create_connection(s.proc_sock.getsockname())
+    e.wait()
+
   def read(s, l=-1):
-    if s.t == "POPEN":
-      select.select([s.p.stdout], [], [])
-      try: 
-        s.flush()
-        r = s.p.stdout.read(l)
-        return r
-      except IOError:
-        return ""
-    elif s.t == "SOCKET":
-      select.select([s.p], [], [])
-      try:
-        if l == -1:
-          l = 16777216
-        return s.p.recv(l)
-      except IOError:
-        return ""
+    select.select([s.p], [], [])
+    try:
+      if l == -1:
+        l = 16777216
+      return s.p.recv(l)
+    except IOError:
+      return ""
 
   def read_until(s, crib):
     r = s.read(len(crib))
@@ -60,45 +62,19 @@ class Tube:
     return s.read_until("\n")
 
   def write(s, x):
-    if s.t == "POPEN":
-      select.select([], [s.p.stdin], [])
-      s.p.stdin.write(x)
-      s.flush()
-    elif s.t == "SOCK":
-      select.select([], [s.p], [])
-      s.p.sendall(x)
+    select.select([], [s.p], [])
+    s.p.sendall(x)
 
   def writeline(s, x):
     s.write(x + "\n")
 
-  def flush(s):
-    if s.t == "POPEN":
-      s.p.stdin.flush()
-      s.p.stdout.flush()
-
   def interact(s, fd=-1):
-    check = "echo \x1b[0;37mGot a shell!\x1b[0m"
-    if s.t == "POPEN":
-      r = ""
-      if fd != -1:
-        m = 'exec /bin/sh <&%d >&%d 2>&%d\n' % (fd, fd, fd)
-        s.writeline(m)
-        s.writeline(check)
-      while True:
-        rfd, wfd, xfd = select.select([s.p.stdout, sys.stdin], [], [])
-        if s.p.stdout in rfd:
-          try:
-            sys.stdout.write(s.read())
-          except EOFError:
-            print '*** Connection closed by remote host ***'
-            break
-        elif sys.stdin in rfd:
-          r = sys.stdin.readline()
-          if not r:
-            break
-          s.write(r)
-          s.flush()
-    elif s.t == "SOCKET":
-      t = telnetlib.Telnet()
-      t.sock = s.p
-      t.interact()
+    check = 'echo -e "\x1b[32mGot a shell!\x1b[0m"\n'
+    t = telnetlib.Telnet()
+    t.sock = s.p
+    if fd != -1:
+      s.write(check)
+      sys.stdout.write(s.read())
+      s.write('exec /bin/sh <&%d >&%d 2>&%d\n' % (fd, fd, fd))
+    t.interact()
+    t.close()

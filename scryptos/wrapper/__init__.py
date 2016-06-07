5e443e23211dc056cb5520b489c711de3ebb650a
Shiho Midorikawa <shiho.elliptic@gmail.com>
2016-06-08 08:26:40 +0900

    add 年賀状2016 writeup

--- scryptos/wrapper/__init__.py
+++ scryptos/wrapper/__init__.py
@@ -1,6 +1,6 @@
 # wrapper
 
 from fplll    import lll,bkz,svp
-from parigp   import parigp
+from parigp   import parigp, set_gp_memalloc_size
 from asm      import asm, disasm
 from singular import Singular

e7f9c2bc0ce4616a1abd4ca6c0ed28976b4dd66b
Shiho Midorikawa <shiho.elliptic@gmail.com>
2016-10-26 19:05:27 +0900

    fix directory

---
+++ test/test_mt19937.py
@@ -0,0 +1,31 @@
+from scryptos.crypto.attack import mt19937
+import random
+import unittest
+
+
+class TestMT19937(unittest.TestCase):
+  def setUp(s):
+    pass
+
+  def test_mt19937(s):
+    seed = 0xdeadbeef
+    mt = mt19937.mt19937(seed)
+    random.seed(seed)
+    s.assertEqual(mt.next(), 956529277)
+    s.assertEqual(mt.next(), 3842322136)
+    s.assertEqual(mt.next(), 3319553134)
+    s.assertEqual(mt.next(), 1843186657)
+    s.assertEqual(mt.next(), 2704993644)
+
+    d = []
+    for _ in xrange(624):
+      d += [random.getrandbits(32)]
+    mt = mt19937.crack(d)
+    for x in xrange(624):
+      s.assertEqual(mt.next(), d[x])
+    for x in xrange(624):
+      s.assertEqual(mt.next(), random.getrandbits(32))
+    for x in xrange(624):
+      mt.prev()
+    for x in d[::-1]:
+      s.assertEqual(mt.prev(), x)

b1148cca7e943aa63536a59d2ac8c0d897b5fde1
Shiho Midorikawa <shiho.elliptic@gmail.com>
2017-03-20 21:06:53 +0900

    [ADD] int2ff/ff2int to pari/gp libs

--- scryptos/wrapper/parigp.py
+++ scryptos/wrapper/parigp.py
@@ -5,7 +5,9 @@ memsize = None
 
 PARI_LIB = [
   'sqrtnall(x,n)={my(V,r,z,r2);r=sqrtn(x,n,&z);if(!z,error("Impossible case in sqrtn"));if(type(x)=="t_INTMOD"||type(x)=="t_PADIC",r2=r*z;n=1;while(r2!=r,r2*=z;n++));V=vector(n);V[1]=r;for(i=2,n,V[i]=V[i-1]*z);V}',
-  'addhelp(sqrtnall,"sqrtnall(x,n):compute the vector of nth-roots of x");',
+  'addhelp(sqrtnall,"sqrtnall(x,n):compute the vector of nth-roots of x")',
+  'int2ff(n,g)={subst(Pol(digits(n,g.p)),\'x,g)}',
+  'ff2int(x)={subst(x.pol,variable(x.pol),x.p)}'
 ]
 
 def parigp(cmd, isAll=False, debug=False):
